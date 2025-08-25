df324428aebca10abdad8f86b76cb0394667609b
Robin <rtyellowgrass@gmail.com>
2025-08-25 18:36:05 +0530

    Integrated sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,28 +4,40 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.sonarqube) apply true
+    alias(libs.plugins.sonarqube) // Important: do not use apply false!
 }
-
 sonarqube {
     properties {
-        property("sonar.projectName", "KMP App")
-        property("sonar.projectKey", "kmp-sq_project")
-        property("sonar.organization", "kmp-sq")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.token", System.getenv("SONAR_TOKEN"))
-
-        property("sonar.language", "kotlin")
-        property("sonar.sourceEncoding", "UTF-8")
-
-        // Fix: Use more specific paths to avoid overlaps
-        property("sonar.sources", "composeApp/src/commonMain/kotlin,shared/src/commonMain/kotlin")
-        property("sonar.tests", "composeApp/src/commonTest/kotlin,shared/src/commonTest/kotlin")
-
-        // Alternative approach - exclude androidMain specifically if you want to include it
-        // property("sonar.sources", "composeApp/src,shared/src")
-        // property("sonar.exclusions", "**/build/**,**/*.gradle.kts,**/androidTest/**")
-
-        property("sonar.exclusions", "**/build/**,**/*.gradle.kts")
+        val koverReport = allprojects.mapNotNull { project ->
+            val reportPath = "${project.projectDir}/build/reports/kover/report.xml"
+            if (File(reportPath).exists()) reportPath else null
+        }.joinToString(",")
+        property("sonar.coverage.jacoco.xmlReportPaths", koverReport)
     }
 }
+tasks.named("sonar") {
+    dependsOn(subprojects.map { it.tasks.named("koverXmlReport") })
+}
+//
+//sonarqube {
+//    properties {
+//        property("sonar.projectName", "KMP App")
+//        property("sonar.projectKey", "kmp-sq_project")
+//        property("sonar.organization", "kmp-sq")
+//        property("sonar.host.url", "https://sonarcloud.io")
+//        property("sonar.token", System.getenv("SONAR_TOKEN"))
+//
+//        property("sonar.language", "kotlin")
+//        property("sonar.sourceEncoding", "UTF-8")
+//
+//        // Fix: Use more specific paths to avoid overlaps
+//        property("sonar.sources", "composeApp/src/commonMain/kotlin,shared/src/commonMain/kotlin")
+//        property("sonar.tests", "composeApp/src/commonTest/kotlin,shared/src/commonTest/kotlin")
+//
+//        // Alternative approach - exclude androidMain specifically if you want to include it
+//        // property("sonar.sources", "composeApp/src,shared/src")
+//        // property("sonar.exclusions", "**/build/**,**/*.gradle.kts,**/androidTest/**")
+//
+//        property("sonar.exclusions", "**/build/**,**/*.gradle.kts")
+//    }
+//}
