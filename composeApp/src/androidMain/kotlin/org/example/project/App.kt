72e20b6a2e087cda73178febb9b9004230e84228
Robin <rtyellowgrass@gmail.com>
2025-08-25 18:45:00 +0530

    Integrated sonarqube

--- composeApp/src/androidMain/kotlin/org/example/project/App.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me R!")
+                Text("Click me Ro!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
