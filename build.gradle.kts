f3c467a7ce44d52c24ea6446892d9e0e8bb2bfce
Robin <rtyellowgrass@gmail.com>
2025-08-25 20:00:02 +0530

    Integrated sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,20 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.sonarqube) // Important: do not use apply false!
+    alias(libs.plugins.kover)
 }
-//sonarqube {
-//    properties {
-//        val koverReport = allprojects.mapNotNull { project ->
-//            val reportPath = "${project.projectDir}/build/reports/kover/report.xml"
-//            if (File(reportPath).exists()) reportPath else null
-//        }.joinToString(",")
-//        property("sonar.coverage.jacoco.xmlReportPaths", koverReport)
-//    }
-//}
-//tasks.named("sonar") {
-//    dependsOn(subprojects.map { it.tasks.named("koverXmlReport") })
-//}
+sonarqube {
+    properties {
+        val koverReport = allprojects.mapNotNull { project ->
+            val reportPath = "${project.projectDir}/build/reports/kover/report.xml"
+            if (File(reportPath).exists()) reportPath else null
+        }.joinToString(",")
+        property("sonar.coverage.jacoco.xmlReportPaths", koverReport)
+    }
+}
+tasks.named("sonar") {
+    dependsOn(subprojects.map { it.tasks.named("koverXmlReport") })
+}
 //
 //sonarqube {
 //    properties {
